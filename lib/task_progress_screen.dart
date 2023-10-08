68784702c7d948fd1c79fb14fcabb294fd05f87f
salamaEnigma <salama92work@gmail.com>
2023-10-08 11:53:10 +0300

    Refactor progress widget

--- lib/task_progress_screen.dart
+++ lib/task_progress_screen.dart
@@ -2,12 +2,28 @@ import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_progress_indicator_v2/liquid_progress_indicator.dart';
 
-class TaskProgressScreen extends StatefulWidget {
+class TaskProgressScreen extends StatelessWidget {
+  const TaskProgressScreen({super.key});
+
   @override
-  _TaskProgressScreenState createState() => _TaskProgressScreenState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Task Progress'),
+      ),
+      body: const ProgressWidget(),
+    );
+  }
 }
 
-class _TaskProgressScreenState extends State<TaskProgressScreen>
+class ProgressWidget extends StatefulWidget {
+  const ProgressWidget({super.key});
+
+  @override
+  State<ProgressWidget> createState() => _ProgressWidgetState();
+}
+
+class _ProgressWidgetState extends State<ProgressWidget>
     with SingleTickerProviderStateMixin {
   // The number of completed tasks
   int _completedTasks = 0;
@@ -28,7 +44,7 @@ class _TaskProgressScreenState extends State<TaskProgressScreen>
     // Initialize the animation controller for the progress indicator
     _progressController = AnimationController(
       vsync: this,
-      duration: Duration(seconds: 1),
+      duration: const Duration(seconds: 1),
       lowerBound: 0.0,
       upperBound: 1.0,
     )..addListener(() {
@@ -65,57 +81,51 @@ class _TaskProgressScreenState extends State<TaskProgressScreen>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Task Progress'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // The liquid progress bar
-            Container(
-              width: 150,
-              height: 150,
-              child: LiquidCircularProgressIndicator(
-                borderColor: Colors.purple,
-                value: _progressController.value,
-                valueColor: const AlwaysStoppedAnimation(Colors.grey),
-                backgroundColor: Colors.white,
-                borderWidth: 10,
-                direction: Axis.vertical,
-                center: Text(
-                  _completedTasks >= 22
-                      ? " اللهم بارك \n \    تمت "
-                      : "${(_completedTasks * (100.toDouble() / 22.toDouble())).round()}%",
-                  style: TextStyle(
-                      color:
-                          _completedTasks >= 22 ? Colors.white : Colors.purple,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
-                      shadows: [
-                        Shadow(
-                            color: Colors.purple,
-                            offset: Offset(1, 1),
-                            blurRadius: 3)
-                      ]),
-                ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // The liquid progress bar
+          SizedBox(
+            width: 150,
+            height: 150,
+            child: LiquidCircularProgressIndicator(
+              borderColor: Colors.purple,
+              value: _progressController.value,
+              valueColor: const AlwaysStoppedAnimation(Colors.grey),
+              backgroundColor: Colors.white,
+              borderWidth: 10,
+              direction: Axis.vertical,
+              center: Text(
+                _completedTasks >= 22
+                    ? " اللهم بارك \n     تمت "
+                    : "${(_completedTasks * (100.toDouble() / 22.toDouble())).round()}%",
+                style: TextStyle(
+                    color: _completedTasks >= 22 ? Colors.white : Colors.purple,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                    shadows: const [
+                      Shadow(
+                          color: Colors.purple,
+                          offset: Offset(1, 1),
+                          blurRadius: 3)
+                    ]),
               ),
             ),
+          ),
 
-            // The confetti widget
-            ConfettiWidget(
-              confettiController: _confettiController,
-              blastDirectionality: BlastDirectionality.explosive,
-            ),
+          // The confetti widget
+          ConfettiWidget(
+            confettiController: _confettiController,
+            blastDirectionality: BlastDirectionality.explosive,
+          ),
 
-            // The button to complete a task
-            ElevatedButton(
-              onPressed: updateProgress,
-              child: Text('Complete Task'),
-            ),
-          ],
-        ),
+          // The button to complete a task
+          ElevatedButton(
+            onPressed: updateProgress,
+            child: const Text('Complete Task'),
+          ),
+        ],
       ),
     );
   }
